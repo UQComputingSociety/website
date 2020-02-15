4e46675bfc6b1ce2014fcdf256c16ff895dc443b
uqcs <contact@uqcs.org.au>
2020-02-15 00:42:02 +0000

    deploy: 7757afb48d537cf717d8897fca03c784ebfa40dd

---
+++ js/navigation.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach(element => {
+            element.addEventListener('click', () => {
+
+                // Get the target from the "data-target" attribute
+                const target = element.dataset.target;
+                const $target = document.getElementById(target);
+
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                element.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+
+            });
+        });
+    }
+
+});
